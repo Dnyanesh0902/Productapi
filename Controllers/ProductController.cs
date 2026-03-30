00a335b7d2f424aadb27e92242d5b79fca5bf00a
dnyaneshwarkokate <Dnyaneshwarkokate@@electropneumatics.com>
2026-03-30 17:29:06 +0530

    ProductAPI

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.Data;
+using System.Threading.Tasks;
+using ProductApi.Models;
+
+namespace ProductApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return Ok(product);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+            var products = _context.Products.Find(id);
+            if (products == null)
+                return BadRequest();
+            products.Name = product.Name;
+            products.Price = product.Price;
+            _context.SaveChanges();
+            return Ok(products);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
